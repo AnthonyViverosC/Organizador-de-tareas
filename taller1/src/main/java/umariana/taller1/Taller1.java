228f50a1d81cec99046349cfeee5e587ef8cefdb
AnthonyViverosC <144851738+AnthonyViverosC@users.noreply.github.com>
2024-02-12 18:49:30 -0500

    Update Taller1.java

--- taller1/src/main/java/umariana/taller1/Taller1.java
+++ taller1/src/main/java/umariana/taller1/Taller1.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package umariana.taller1;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
